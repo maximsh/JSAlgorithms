2a956603934e75cd6c4a946a1b3b9a1fe1dc88c7
Maxim Shkolyarenko <maxim.shkolyarenko@gmail.com>
2019-12-25 15:02:39 +0200

    Add tests for NaN values

--- test/core/compare.test.js
+++ test/core/compare.test.js
@@ -14,6 +14,26 @@ test('negative is less than positive', () => {
     }
 });
 
+test('any number less than NaN', () => {
+    expect(compare(-Math.random(), NaN)).toEqual(-1);
+    expect(compare(Math.random(), NaN)).toEqual(-1);
+    expect(compare(-Infinity, NaN)).toEqual(-1);
+    expect(compare(Infinity, NaN)).toEqual(-1);
+    expect(compare(0, NaN)).toEqual(-1);
+});
+
+test('NaN is great than any number', () => {
+    expect(compare(NaN, -Math.random())).toEqual(1);
+    expect(compare(NaN, Math.random())).toEqual(1);
+    expect(compare(NaN, -Infinity)).toEqual(1);
+    expect(compare(NaN, Infinity)).toEqual(1);
+    expect(compare(NaN, 0)).toEqual(1);
+});
+
+test('NaN is equal NaN', () => {
+    expect(compare(NaN, NaN)).toEqual(0);
+});
+
 test('the number is equal to itself', () => {
     for (let i = 0; i < 100; i++) {
         const a = Math.random();
