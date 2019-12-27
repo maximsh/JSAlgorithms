186b0d69ac7f51c923da382b612aea4aeb7c809e
Maxim Shkolyarenko <maxim.shkolyarenko@gmail.com>
2019-12-27 20:45:04 +0200

    Implement merge sort

---
+++ src/sorting/merge.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+
+import compare from '../core/compare.js';
+import bubble from './bubble.js';
+
+function sort(data, compareFunction = compare) {
+	if (data.length < 4) {
+		bubble(data, compareFunction);
+	} else {
+		const center = data.length >> 1;
+		const left = data.slice(0, center);
+		const right = data.slice(center);
+		let mainPos = 0;
+		let leftPos = 0;
+		let rightPos = 0;
+		sort(left, compareFunction);
+		sort(right, compareFunction);
+		while (leftPos < left.length && rightPos < right.length) {
+			data[mainPos++] = compareFunction(left[leftPos], right[rightPos]) < 0 ? left[leftPos++] : right[rightPos++];
+		}
+		while (leftPos < left.length) {
+			data[mainPos++] = left[leftPos++];
+		}
+		while (rightPos < right.length) {
+			data[mainPos++] = right[rightPos++];
+		}
+	}
+
+	return data;
+}
+
+export default sort;
---
+++ test/sorting/merge.test.js
@@ -0,0 +1,6 @@
+/*jshint esversion: 6 */
+
+import sort from '../../src/sorting/merge.js';
+import testing from './testing.js';
+
+testing(sort);
