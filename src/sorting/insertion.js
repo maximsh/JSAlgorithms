77bd5d82f7fd8071be8cfba24da229098c46afa6
Maxim Shkolyarenko <maxim.shkolyarenko@gmail.com>
2020-01-07 14:13:14 +0200

    Code review

--- src/sorting/insertion.js
+++ src/sorting/insertion.js
@@ -3,9 +3,10 @@
 import compare from '../core/compare.js';
 
 function sort(data, compareFunction = compare) {
+	const size = data.length;
 	let bufferSize = 1;
 
-	while (bufferSize < data.length) {
+	while (bufferSize < size) {
 		let index = bufferSize;
 		while (index >= 1 && compareFunction(data[index-1], data[index]) > 0) {
 			[data[index-1], data[index]] = [data[index], data[index-1]];
--- src/sorting/quick.js
+++ src/sorting/quick.js
@@ -1,7 +1,6 @@
 /*jshint esversion: 6 */
 
 import compare from '../core/compare.js';
-import gnome from './gnome.js';
 
 function sort(data, compareFunction = compare) {
 	const size = data.length;
@@ -9,7 +8,7 @@ function sort(data, compareFunction = compare) {
 		return data;
 	}
 
-	if (data.length < 4) {
+	if (size < 4) {
 		if (compareFunction(data[0], data[1]) > 0) {
 			[data[0], data[1]] = [data[1], data[0]];
 		}
@@ -23,7 +22,7 @@ function sort(data, compareFunction = compare) {
 		}
 	} else {
 		const results = [[], [], []];
-		const avg = data[data.length >> 1];
+		const avg = data[size >> 1];
 		for (const item of data) {
 			results[compareFunction(item, avg) + 1].push(item);
 		}
