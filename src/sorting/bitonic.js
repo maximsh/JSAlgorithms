9be256abbbb1cceb74f5a99f2168c7e48cdc1406
Maxim Shkolyarenko <maxim.shkolyarenko@gmail.com>
2020-01-07 14:14:09 +0200

    Fix bitonic sort

--- src/sorting/bitonic.js
+++ src/sorting/bitonic.js
@@ -2,49 +2,34 @@
 
 import compare from '../core/compare.js';
 
-function sort(data, compareFunction = compare, direction = 1) {
-	function merge(begin, size, direction) {
-		const end = begin + size;
-		let step = size >> 1;
-		for (let index = begin + step; index < end; index++) {
-			if (compareFunction(data[index-step], data[index]) === direction) {
-				[data[index-step], data[index]] = [data[index], data[index-step]];
+function sort(data, compareFunction = compare) {
+	function bitonicMerge(begin, size, direction) {
+		if (size > 1) {
+			let step = 1;
+			while (step < size) {
+				step = step << 1;
 			}
-		}
-		if (size > 3) {
-			merge(begin + 0, step, direction);
-			merge(begin + step, size - step, direction);
+			step = step >> 1;
+			for (let index = begin; index < begin + size - step; index++) {
+				if (compareFunction(data[index], data[index+step]) === direction) {
+					[data[index], data[index+step]] = [data[index+step], data[index]];
+				}
+			}
+			bitonicMerge(begin, step, direction);
+			bitonicMerge(begin + step, size - step, direction);
 		}
 	}
 
-	function split(begin, size, direction) {
-		if (size < 2) {
-			return;
-		}
-
-		if (size < 4) {
-			const second = begin + 1;
-			const third = begin + 2;
-			if (compareFunction(data[begin], data[second]) === direction) {
-				[data[begin], data[second]] = [data[second], data[begin]];
-			}
-			if (size === 3) {
-				if (compareFunction(data[second], data[third]) === direction) {
-					[data[second], data[third]] = [data[third], data[second]];
-				}
-				if (compareFunction(data[begin], data[second]) === direction) {
-					[data[begin], data[second]] = [data[second], data[begin]];
-				}
-			}
-		} else {
+	function bitonicSort(begin, size, direction) {
+		if (size > 1) {
 			const half = size >> 1;
-			split(begin, half, direction);
-			split(begin + half, size - half, -direction);
-			merge(begin, size, direction);
+			bitonicSort(begin, half, -direction);
+			bitonicSort(begin + half, size - half, direction);
+			bitonicMerge(begin, size, direction);
 		}
 	}
 
-	split(0, data.length, direction);
+	bitonicSort(0, data.length, 1);
 
 	return data;
 }
